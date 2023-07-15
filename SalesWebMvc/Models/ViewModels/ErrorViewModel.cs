962e892222957a10e4498ee4b621d38b85d2ff5a
Vinicius Fernandes <estevesf.vinicius@gmail.com>
2023-07-15 14:36:50 -0300

    Criação do Projeto e ViewModel Departament

---
+++ SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using SalesWebMvc.Models.ViewModels;
+
+namespace SalesWebMvc.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
